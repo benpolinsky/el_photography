97cc1246528a55dae1bd6c40f42b31e23ecb188b
Ben Polinsky <benjamin.polinsky@gmail.com>
2016-06-10 16:22:19 -0400

    this is slwo

--- app/assets/javascripts/admin/forms.js
+++ app/assets/javascripts/admin/forms.js
@@ -1,14 +1,15 @@
 $(document).ready(function() {
-  var loader = require("./loader");
-  window.loader = loader;
+  let Loader = require("loader");
+  var new_loader = new Loader();
+  var submitting = false;
   $('form.patience').on('submit', function(event) {
-    if (typeof submitting == 'undefined') {
-      loader.start();
+    if (submitting == false) {
+      new_loader.start();
       submitting = true;
       $(this).submit();
       return false      
     } else {
-      submitting = undefined;
+      submitting = false;
     }
   });
 });
--- app/assets/javascripts/loader.js
+++ app/assets/javascripts/loader.js
@@ -1,9 +1,24 @@
-module.exports = {
-  start: function () {
-    $('span.loader').addClass('active');      
-  },
-  
-  stop: function () {
-    $('span.loader').removeClass('active');
+class Loader {
+  constructor(element){
+    this.element = document.body;
+    this.id = new Date().getTime();
+    this.append_template();
+  }
+  loader_element(){
+    this.the_element = document.getElementById(this.id);
+  }
+  start() {
+    this.the_element.classList.add('active');
+  }
+  stop(){
+    loader_element().classList.remove('active');
+  }
+  append_template(){
+    let parent = this.element;
+    var template = document.createElement("span");
+    template.classList.add('loader', 'fa', 'fa-spinner', 'fa-spin');
+    parent.appendChild(template);
   }
 }
+
+module.exports = Loader
