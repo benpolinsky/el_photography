7ef0fc03466d6069d2648e21393345bf0caf785d
Ben Polinsky <benjamin.polinsky@gmail.com>
2016-07-05 15:05:34 -0400

    dont serve static assets from cloudfront... uet

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,7 +32,6 @@ Rails.application.configure do
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  config.action_controller.asset_host = "https://d32mcea23qtv9h.cloudfront.net/"
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
@@ -93,7 +92,7 @@ Rails.application.configure do
     :authentication => :plain,
   }
   config.action_mailer.default_url_options = { :host => "elliotpolinsky.com" }
-  config.action_mailer.asset_host = "https://d32mcea23qtv9h.cloudfront.net/"
+
 
 
 end
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,6 +6,7 @@ unless Rails.env.test?
       aws_secret_access_key: ENV['aws_access_secret'],                        # required
       region:                'us-west-2'               # optional, defaults to nil
     }
+    config.asset_host = "https://d32mcea23qtv9h.cloudfront.net/"
     config.fog_directory  = 'elpphotography'                     # required
   end
 end
